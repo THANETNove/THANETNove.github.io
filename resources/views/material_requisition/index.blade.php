a74627e069dffb598da6c83585d0f264c37168a6
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-11-30 13:49:25 +0700

    test code

--- resources/views/material_requisition/index.blade.php
+++ resources/views/material_requisition/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Content -->
-
+    {{-- test upcode --}}
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="row">
             <div class="col-12 mb-4 order-0">
