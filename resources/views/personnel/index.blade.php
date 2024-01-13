b9c020abd411a07065c67d73f0ef34ec81a527a4
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2024-01-13 20:09:38 +0700

    เเก้

--- resources/views/personnel/index.blade.php
+++ resources/views/personnel/index.blade.php
@@ -38,14 +38,23 @@
                                             $i = 1;
                                         @endphp
                                         <tbody class="table-border-bottom-0">
-                                            @foreach ($data->where('status', '2')->sortByDesc('created_at') as $da)
+                                            @foreach ($data->sortByDesc('created_at') as $da)
                                                 <tr>
                                                     <th scope="row">{{ $i++ }}</th>
                                                     <td>{{ $da->employee_id }}</td>
                                                     <td>{{ $da->prefix }} {{ $da->first_name }} {{ $da->last_name }}</td>
                                                     <td>{{ $da->phone_number }}</td>
                                                     <td>{{ $da->department_name }}</td>
-                                                    <td>หัวหน้าวัสดุ</td>
+                                                    <td>
+                                                        @if ($da->status == 0)
+                                                            ผู้เบิก
+                                                        @elseif ($da->status == 1)
+                                                            เจ้าหน้าที่วัสดุ
+                                                        @else
+                                                            หัวหน้าวัสดุ
+                                                        @endif
+
+                                                    </td>
                                                     <td>
                                                         @if ($da->statusEmployee == 'on')
                                                             <span class="badge bg-label-success me-1">พนักงาน</span>
@@ -54,18 +63,19 @@
                                                         @endif
 
                                                     </td>
-                                                    @if (Auth::user()->status == '2')
-                                                        <td>
-                                                            <div class="dropdown">
-                                                                <button type="button"
-                                                                    class="btn p-0 dropdown-toggle hide-arrow"
-                                                                    data-bs-toggle="dropdown">
-                                                                    <i class="bx bx-dots-vertical-rounded"></i>
-                                                                </button>
-                                                                <div class="dropdown-menu">
-                                                                    <a class="dropdown-item"
-                                                                        href="{{ url('personnel-show', $da->id) }}"><i
-                                                                            class='bx bxs-show'></i> View</a>
+
+                                                    <td>
+                                                        <div class="dropdown">
+                                                            <button type="button"
+                                                                class="btn p-0 dropdown-toggle hide-arrow"
+                                                                data-bs-toggle="dropdown">
+                                                                <i class="bx bx-dots-vertical-rounded"></i>
+                                                            </button>
+                                                            <div class="dropdown-menu">
+                                                                <a class="dropdown-item"
+                                                                    href="{{ url('personnel-show', $da->id) }}"><i
+                                                                        class='bx bxs-show'></i> View</a>
+                                                                @if (Auth::user()->status == '2')
                                                                     @if ($da->statusEmployee == 'on')
                                                                         <a class="dropdown-item"
                                                                             href="{{ url('personnel-edit', $da->id) }}"><i
@@ -81,118 +91,15 @@
                                                                             <i class='bx bx-up-arrow-circle'></i> update
                                                                             status</a>
                                                                     @endif
-
-                                                                </div>
+                                                                @endif
                                                             </div>
-                                                        </td>
-                                                    @endif
+                                                        </div>
+                                                    </td>
+
                                                 </tr>
                                             @endforeach
 
-                                            @foreach ($data->where('status', '1')->sortByDesc('created_at') as $da)
-                                                <tr>
-                                                    <th scope="row">{{ $i++ }}</th>
-                                                    <td>{{ $da->employee_id }}</td>
-                                                    <td>{{ $da->prefix }} {{ $da->first_name }} {{ $da->last_name }}
-                                                    </td>
-                                                    <td>{{ $da->phone_number }}</td>
-                                                    <td>{{ $da->department_name }}</td>
-                                                    <td>เจ้าหน้าที่วัสดุ</td>
-                                                    <td>
-                                                        @if ($da->statusEmployee == 'on')
-                                                            <span class="badge bg-label-success me-1">พนักงาน</span>
-                                                        @else
-                                                            <span class="badge bg-label-warning me-1">พ้นสภาพพนักงาน</span>
-                                                        @endif
 
-                                                    </td>
-                                                    @if (Auth::user()->status == '2')
-                                                        <td>
-                                                            <div class="dropdown">
-                                                                <button type="button"
-                                                                    class="btn p-0 dropdown-toggle hide-arrow"
-                                                                    data-bs-toggle="dropdown">
-                                                                    <i class="bx bx-dots-vertical-rounded"></i>
-                                                                </button>
-                                                                <div class="dropdown-menu">
-                                                                    <a class="dropdown-item"
-                                                                        href="{{ url('personnel-show', $da->id) }}"><i
-                                                                            class='bx bxs-show'></i> View</a>
-                                                                    @if ($da->statusEmployee == 'on')
-                                                                        <a class="dropdown-item"
-                                                                            href="{{ url('personnel-edit', $da->id) }}"><i
-                                                                                class="bx bx-edit-alt me-1"></i> Edit</a>
-                                                                        @if (Auth::user()->id != $da->id)
-                                                                            <a class="dropdown-item"
-                                                                                href="{{ url('personnel-destroy', $da->id) }}"><i
-                                                                                    class="bx bx-trash me-1"></i> Delete</a>
-                                                                        @endif
-                                                                    @else
-                                                                        <a class="dropdown-item"
-                                                                            href="{{ url('personnel-update-status', $da->id) }}">
-                                                                            <i class='bx bx-up-arrow-circle'></i> update
-                                                                            status</a>
-                                                                    @endif
-
-                                                                </div>
-                                                            </div>
-                                                        </td>
-                                                    @endif
-                                                </tr>
-                                            @endforeach
-
-                                            @foreach ($data->where('status', '0')->sortByDesc('created_at') as $da)
-                                                <tr>
-                                                    <th scope="row">{{ $i++ }}</th>
-                                                    <td>{{ $da->employee_id }}</td>
-                                                    <td>{{ $da->prefix }} {{ $da->first_name }} {{ $da->last_name }}
-                                                    </td>
-                                                    <td>{{ $da->phone_number }}</td>
-                                                    <td>{{ $da->department_name }}</td>
-                                                    <td>ผู้เบิก</td>
-                                                    <td>
-                                                        @if ($da->statusEmployee == 'on')
-                                                            <span class="badge bg-label-success me-1">พนักงาน</span>
-                                                        @else
-                                                            <span class="badge bg-label-warning me-1">พ้นสภาพพนักงาน</span>
-                                                        @endif
-
-                                                    </td>
-                                                    @if (Auth::user()->status == '2')
-                                                        <td>
-                                                            <div class="dropdown">
-                                                                <button type="button"
-                                                                    class="btn p-0 dropdown-toggle hide-arrow"
-                                                                    data-bs-toggle="dropdown">
-                                                                    <i class="bx bx-dots-vertical-rounded"></i>
-                                                                </button>
-                                                                <div class="dropdown-menu">
-                                                                    <a class="dropdown-item"
-                                                                        href="{{ url('personnel-show', $da->id) }}"><i
-                                                                            class='bx bxs-show'></i> View</a>
-                                                                    @if ($da->statusEmployee == 'on')
-                                                                        <a class="dropdown-item"
-                                                                            href="{{ url('personnel-edit', $da->id) }}"><i
-                                                                                class="bx bx-edit-alt me-1"></i> Edit</a>
-                                                                        @if (Auth::user()->id != $da->id)
-                                                                            <a class="dropdown-item"
-                                                                                href="{{ url('personnel-destroy', $da->id) }}"><i
-                                                                                    class="bx bx-trash me-1"></i> Delete</a>
-                                                                        @endif
-                                                                    @else
-                                                                        <a class="dropdown-item"
-                                                                            href="{{ url('personnel-update-status', $da->id) }}">
-                                                                            <i class='bx bx-up-arrow-circle'></i>
-                                                                            update
-                                                                            status</a>
-                                                                    @endif
-
-                                                                </div>
-                                                            </div>
-                                                        </td>
-                                                    @endif
-                                                </tr>
-                                            @endforeach
                                         </tbody>
                                     </table>
                                     <div class="mt-5">
