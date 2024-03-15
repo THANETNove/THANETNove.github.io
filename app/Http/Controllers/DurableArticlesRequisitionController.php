98b113bd5f312e5313bd9b7eba3b3bca489fdb4e
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2024-03-15 11:56:17 +0700

    ลบ

--- app/Http/Controllers/DurableArticlesRequisitionController.php
+++ app/Http/Controllers/DurableArticlesRequisitionController.php
@@ -314,18 +314,39 @@ class DurableArticlesRequisitionController extends Controller
      */
     public function destroy(string $id)
     {
-        $data_requisition = DurableArticlesRequisition::find($id);
-        $data = DB::table('durable_articles')
-        ->where('code_DurableArticles', $data_requisition->durable_articles_id)
-        ->get();
+        $data = DurableArticlesRequisition::find($id);
 
 
-        DurableArticles::where('code_DurableArticles', $data_requisition->durable_articles_id)->update([
-            'remaining_amount' =>  $data[0]->remaining_amount + $data_requisition->amount_withdraw,
-        ]);
-        DurableArticlesRequisition::where('id', $id)->update([
-            'status' =>  "1",
-        ]);
+        $dataRequisitions = DB::table('durable_articles_requisitions')
+        ->where('durable_articles_requisitions.group_withdraw', $data->group_withdraw)
+        ->join('users', 'durable_articles_requisitions.id_user', '=', 'users.id')
+        ->leftJoin('departments', 'users.department_id', '=', 'departments.id')
+        ->leftJoin('durable_articles', 'durable_articles_requisitions.group_id', '=', 'durable_articles.id')
+        ->leftJoin('type_categories', 'durable_articles.type_durableArticles', '=', 'type_categories.id')
+        ->leftJoin('categories', 'durable_articles.group_class', '=', 'categories.id')
+        ->leftJoin('storage_locations', 'durable_articles.code_material_storage', '=', 'storage_locations.code_storage')
+        ->select('durable_articles_requisitions.*','type_categories.type_name','type_categories.type_code', 'users.prefix', 'users.first_name','users.last_name','departments.department_name',
+        'durable_articles.durableArticles_name','durable_articles.warranty_period','durable_articles.description','durable_articles.group_count','durable_articles.durableArticles_number','categories.category_name','categories.category_code','storage_locations.building_name','storage_locations.floor','storage_locations.room_name')
+        ->orderBy('durable_articles_requisitions.id','DESC');
+
+        $requisitions = $dataRequisitions->get();
+        $number = $dataRequisitions->count();
+
+
+
+           for ($i = 0; $i < $number ; $i++) {
+
+
+            DurableArticles::where('id', $requisitions[$i]->group_id)->update([
+                'remaining_amount' =>  1,
+              ]);
+
+            DurableArticlesRequisition::where('id', $requisitions[$i]->id)->update([
+                'status' =>  1,
+            ]);
+
+            }
+
         return redirect('durable-articles-requisition-index')->with('message', "ยกเลิกสำเร็จ");
     }
 
