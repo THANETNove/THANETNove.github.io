39d4b3a646cb30d91405015244e5b79a9b667457
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2024-04-30 15:55:53 +0700

    ggdh

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -52,7 +52,44 @@
                     ->orderBy('bet_distributions.id', 'DESC')
                     ->get();
 
-                $countData = $dataDuAc->count() + $dataBet->count();
+                $dataReq = DB::table('durable_articles_requisitions')
+                    ->where('durable_articles_requisitions.status', 2)
+                    ->where('durable_articles_requisitions.statusApproval', 1)
+                    ->join('users', 'durable_articles_requisitions.id_user', '=', 'users.id')
+                    ->leftJoin('departments', 'users.department_id', '=', 'departments.id')
+                    ->leftJoin('durable_articles', 'durable_articles_requisitions.group_id', '=', 'durable_articles.id')
+                    ->leftJoin('type_categories', 'durable_articles.type_durableArticles', '=', 'type_categories.id')
+                    ->leftJoin('categories', 'durable_articles.group_class', '=', 'categories.id')
+                    ->leftJoin(
+                        'storage_locations',
+                        'durable_articles.code_material_storage',
+                        '=',
+                        'storage_locations.code_storage',
+                    )
+                    ->select(
+                        'durable_articles_requisitions.*',
+                        'type_categories.type_name',
+                        'type_categories.type_code',
+                        'users.prefix',
+                        'users.first_name',
+                        'users.last_name',
+                        'departments.department_name',
+                        'durable_articles.durableArticles_name',
+                        'durable_articles.warranty_period',
+                        'durable_articles.description',
+                        'durable_articles.group_count',
+                        'durable_articles.durableArticles_number',
+                        'categories.category_name',
+                        'categories.category_code',
+                        'storage_locations.building_name',
+                        'storage_locations.floor',
+                        'storage_locations.room_name',
+                    )
+
+                    ->orderBy('durable_articles_requisitions.id', 'DESC')
+                    ->get();
+
+                $countData = $dataDuAc->count() + $dataBet->count() + $dataReq->count();
 
             @endphp
             <!-- Place this tag where you want the button to render. -->
@@ -87,6 +124,18 @@
 
                                 <i class='bx warn-2'>{{ $dataBet->count() }}</i>
                                 <span class="align-middle">อนุมัติครุภัณฑ์จำหน่าย</span>
+                            </a>
+                        </li>
+                    @endif
+                    @if ($dataReq->count() > 0)
+                        <li>
+                            <div class="dropdown-divider"></div>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ url('return-item-index') }}">
+
+                                <i class='bx warn-2'>{{ $dataReq->count() }}</i>
+                                <span class="align-middle">คึนครุภัณฑ์</span>
                             </a>
                         </li>
                     @endif
