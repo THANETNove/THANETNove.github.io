e1faec05aa8b580f514cfe1917790e8bcfa25b51
THANETNove <novel2020manga@gmail.com>
2024-06-10 08:25:15 +0700

    เเก้

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,6 +1,6 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-        <a href="index.html" class="app-brand-link">
+        <a href="{{ url('home') }}" class="app-brand-link">
             <span class="app-brand-logo demo">
                 <img src="{{ URL::asset('/assets/img/icons/unicons/logo.png') }}" alt
                     class="w-px-50 h-auto rounded-circle" />
