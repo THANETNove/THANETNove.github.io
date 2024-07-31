bdfff354ae59641711b226d281a6420ff9d86b17
THANETNove <novel2020manga@gmail.com>
2024-07-31 21:48:48 +0700

    add

--- app/Http/Controllers/ReturnItemController.php
+++ app/Http/Controllers/ReturnItemController.php
@@ -253,6 +253,54 @@ class ReturnItemController extends Controller
         return redirect('durable-articles-requisition-index')->with('message', "รอการอนุมัติคืนครุภัณฑ์");
     }
 
+    public function durableRequisitionReturnList(string $id)
+    {
+        $data = DurableArticlesRequisition::find($id);
+
+        $dataRequisitions = DB::table('durable_articles_requisitions')
+            ->where('durable_articles_requisitions.id', $id)
+            ->join('users', 'durable_articles_requisitions.id_user', '=', 'users.id')
+            ->leftJoin('departments', 'users.department_id', '=', 'departments.id')
+            ->leftJoin('durable_articles', 'durable_articles_requisitions.group_id', '=', 'durable_articles.id')
+            ->leftJoin('type_categories', 'durable_articles.type_durableArticles', '=', 'type_categories.id')
+            ->leftJoin('categories', 'durable_articles.group_class', '=', 'categories.id')
+            ->leftJoin('storage_locations', 'durable_articles.code_material_storage', '=', 'storage_locations.code_storage')
+            ->select(
+                'durable_articles_requisitions.*',
+                'type_categories.type_name',
+                'type_categories.type_code',
+                'users.prefix',
+                'users.first_name',
+                'users.last_name',
+                'departments.department_name',
+                'durable_articles.durableArticles_name',
+                'durable_articles.warranty_period_start',
+                'durable_articles.warranty_period_end',
+                'durable_articles.description',
+                'durable_articles.group_count',
+                'durable_articles.durableArticles_number',
+                'categories.category_name',
+                'categories.category_code',
+                'storage_locations.building_name',
+                'storage_locations.floor',
+                'storage_locations.room_name'
+            )
+            ->orderBy('durable_articles_requisitions.id', 'DESC');
+
+        $requisitions = $dataRequisitions->get();
+        $number = $dataRequisitions->count();
+
+        for ($i = 0; $i < $number; $i++) {
+
+            DurableArticlesRequisition::where('id', $requisitions[$i]->id)->update([
+                'status' =>  2,
+            ]);
+        }
+
+
+        return redirect('durable-articles-requisition-index')->with('message', "รอการอนุมัติคืนครุภัณฑ์");
+    }
+
     public function durableRequisitionReturnApproval(string $id)
     {
         $data = DurableArticlesRequisition::find($id);
